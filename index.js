b126b53e00ee88305718fbf0e73663703dde103e
wilson <wilson.brochero@i2btech.com>
2020-10-10 17:42:50 -0500

    primer commit

---
+++ index.js
@@ -0,0 +1,16 @@
+var http = require('http');
+
+var server = http.createServer();
+
+function mensaje(petic, resp) {
+    resp.writeHead(200, {'content-type': 'text/plain'});
+    resp.write('Hola Mundo');
+    resp.end();
+}
+
+server.on('request', mensaje);
+
+
+server.listen(3000, function () {
+    console.log('La Aplicación está funcionando en el puerto 3000');
+ });
